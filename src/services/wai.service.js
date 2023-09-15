a1541810fe4d3fa180f1aa572b3c93bd05ea3b7b
NIRANJAN B <niranjannb7777@gmail.com>
2023-09-16 03:36:33 +0530

    Workflow from prompt

--- src/services/wai.service.js
+++ src/services/wai.service.js
@@ -1,16 +1,15 @@
 import axios from 'axios';
-import { waiPrompt } from '../config/wai.js';
 
 /**
  *
  * @param {object} data - {text : text}
  * @returns {Promise} List of Nodes
  */
-const url = 'http://192.168.3.117:5000/chatgpt';
+const url = 'http://192.168.3.117:5000/wai';
 
 const getWAI = async request => {
-  const result = await axios.post(url, { text: waiPrompt + request.text });
-  return result.data.text.trim().split(/,/);
+  const result = await axios.post(url, { text: request.text });
+  return result.data;
 };
 
 export {
