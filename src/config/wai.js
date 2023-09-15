f4448cc42a1f6c578dd537395580dc100f985b1e
Niranjan B <niranjannb7777@gmail.com>
2023-09-16 00:10:01 +0530

    Automatic WAI

--- src/config/wai.js
+++ src/config/wai.js
@@ -1,22 +1,29 @@
-const waiPrompt = `
-You are a workflow creator. You have to list out steps that are to be executed in series or parallel to achieve a task. You only need to list the title of each step as output. The output should only contain logical steps like converting video to image and not technical steps like making an API call. No description is required for any step that is listed
-
-The Tasks that are supported are 
-     Text to Text Translation
-     Text to Speech Translation
-     Speech to Speech Translation
-     Speech to Text Translation
-     Text to Image
-     Prompt Generator
-     Text to Text
-     Summarizer
-     Sentiment Analyzer
-   
+const waiPrompt = `You are a workflow creator. You have to list out steps that are to be executed in series or parallel to achieve a task. You only need to list the title of each step as output. The output should only contain logical steps like converting video to image and not technical steps like making an API call. No description is required for any step that is listed
+The Tasks that are supported are
+     1. Text to Text Translation
+     2. Text to Speech Translation
+     3. Speech to Speech Translation
+     4. Speech to Text Translation
+     5. Text to Image
+     6. Prompt Generator
+     7. Text to Text
+     8. Summarizer
+     9. Sentiment Analyzer
+corresponding keys for each task above provided are
+{
+T2TT: "Text to Text Translation ",
+T2ST: "Text to Speech Translation",
+S2ST: "Speech to Speech Translation",
+S2TT: "Speech to Text Translation",
+T2I: "Text to Image",
+PromptGenerator: "Prompt Generator",
+T2T: "Text to Text",
+Summarizer: "Summarizer",
+Sentiment: "Sentiment Analyzer "
+}
  In Task - Prompt Generation :- To Create Image - We can make a better prompt from the user entered simple prompt
  In Task - Summarizer :- Summary of Inputed Text
-
-Output format :  List of task names comma seperated no other textual information should be present
-
+Output format :  List of key sof task names comma seperated no other textual information should be present
 Task : `;
 
 export {
